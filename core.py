12c98f37db40873c58582e31a99fd275e43b0d8d
alfeuduran <alfeuduran@gmail.com>
2023-09-21 20:44:45 -0300

    add some modifications and encapsulate the chat in a mathod

--- core.py
+++ core.py
@@ -5,37 +5,30 @@ from langchain.embeddings import OpenAIEmbeddings
 import os
 import logging
 
-
-
 logger = logging.getLogger(__name__)
 
-model: str = "text-embedding-ada-002"
-vector_store_address: str = "https://gptkb-rahzjehw7exti.search.windows.net"
-vector_store_password: str = "KB7fjV9u8X4WWuF94enaPwmaCFCDgqgZeKZoEsz0WKAzSeCEt1Nl"
-index_name = "langchain-index-helper-final"
+vector_store_address: str = os.getenv("AZURE_SEARCH_SERVICE_ENDPOINT")
+vector_store_password: str = os.getenv("AZURE_SEARCH_ADMIN_KEY")
+index_name : str = os.getenv("INDEX_NAME")
 
 
-embeddings: OpenAIEmbeddings = OpenAIEmbeddings(openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),deployment="embedding", chunk_size=200, openai_api_base=os.getenv("AZURE_OPENAI_ENDPOINT"), openai_api_type="azure",model="text-embedding-ada-002" )
+def run_llm(query: str) -> any:
+
+    embeddings: OpenAIEmbeddings = OpenAIEmbeddings(openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),deployment="embedding", chunk_size=200, openai_api_base=os.getenv("AZURE_OPENAI_ENDPOINT"), openai_api_type="azure",model="text-embedding-ada-002" )
+    vector_store: AzureSearch = AzureSearch(
+        azure_search_endpoint=vector_store_address,
+        azure_search_key=vector_store_password,
+        index_name=index_name,
+        embedding_function=embeddings.embed_query, 
+    )
+
+    chat = AzureChatOpenAI(verbose=True,temperature=0,openai_api_base=os.getenv("AZURE_OPENAI_ENDPOINT"),openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),deployment_name="chat")
+    qa = RetrievalQA.from_chain_type(llm=chat,chain_type="stuff",retriever=vector_store.as_retriever(), return_source_documents=True)
+
+    return qa({"query": query})
 
 
-# podemos criar um método aqui chamado run_llm onde ele recebe a query e retorna o resultado pelo chat
-
-index_name = "langchain-index-helper-final"
-vector_store: AzureSearch = AzureSearch(
-    azure_search_endpoint=vector_store_address,
-    azure_search_key=vector_store_password,
-    index_name=index_name,
-    embedding_function=embeddings.embed_query, 
-)
-
-chat = AzureChatOpenAI(verbose=True,temperature=0,openai_api_base=os.getenv("AZURE_OPENAI_ENDPOINT"),openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"))
-qa = RetrievalQA.from_chain_type(llm=chat,chain_type="stuff",retriever=vector_store.as_retriever(), return_source_documents=True)
+if __name__ == "__main__":
+    print(run_llm("What are the use cases for langchain"))
 
 
-print(qa("What is module in Langchain?"))
-
-
-
-# def runllm(query: str) -> any:
-#     #  embeddings new
-#     # add vectorstore connection again? 
